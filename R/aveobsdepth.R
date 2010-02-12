ab5ae959a338514a2233e74419a041c861fe1b28
steingod <steingod>
2010-02-12 15:50:19 +0000

    Temporary storage while developing...

--- R/aveobsdepth.R
+++ R/aveobsdepth.R
@@ -29,7 +29,7 @@
 # NA
 #
 # CVS_ID:
-# $Id: aveobsdepth.R,v 1.2 2010-02-10 09:27:52 steingod Exp $
+# $Id: aveobsdepth.R,v 1.3 2010-02-12 15:50:19 steingod Exp $
 #  
 
 aveobsdepth <- function(x,depth,method="extract") {
@@ -54,18 +54,39 @@ aveobsdepth <- function(x,depth,method="extract") {
 ##		    temperature=x$data$temperature[x$data$depth<=depth],
 ##		    salinity=x$data$salinity[x$data$depth<=depth]
 ##		    ))
-##	myindex <- factor(format(t$data$time,"%Y%m%d","GMT"))
-##	maxdepth <- tapply(t$data$depth,myindex,max,na.rm=T)
-##	mindepth <- tapply(t$data$depth,myindex,minna.rm=T)
-##	ndepths <- tapply(t$data$depth,myindex,length)
+	#myindex <- factor(paste(format(t$data$time,"%Y%m%d","GMT"),
+	#	    t$data$depth,sep="_"))
+	#mytime <- ISOdatetime(1970,1,1,0,0,0,"GMT")+tapply(t$data$time,myindex,mean,na.rm=T)
+	#mydepth <- 
+	#mytemp <- tapply(t$data$temperature,myindex,mean,na.rm=T)
+	#mypsal <- tapply(t$data$salinity,myindex,mean,na.rm=T)
+
+	t <- x
+	myindex <- factor(paste(format(t$data$time,"%Y%m%d","GMT"),
+		    t$data$depth,sep="_"))
+##	tmp <- by(t$data, 
+##		myindex, 
+##		mean, na.rm=TRUE)
+	tmp <- aggregate(t$data,list(myindex), mean, na.rm=T)
+	return(tmp)
+
+	return(list(t=mytime,d=mydepth,m=mytemp,s=mypsal))
+
+
+	maxdepth <- tapply(t$data$depth,myindex,max,na.rm=T)
+	mindepth <- tapply(t$data$depth,myindex,minna.rm=T)
+	ndepths <- tapply(t$data$depth,myindex,length)
+
+	return(list(a=myindex,b=maxdepth,c=mindepth,d=ndepths))
 
 	t <- x
 	x <- t$data[do.call(order,t$data[,c("time","depth")]),]
-	tmp <- by(t$data[,c("temperature","depth")], t$data$time, function(u) weighted.mean((u$temperature[-1]+u$temperature[-nrow(u)])/2, w=diff(u$depth)/depth, na.rm=TRUE))
+	tmp <- by(t$data[,c("temperature","depth")], 
+		t$data$time, 
+		function(u) weighted.mean((u$temperature[-1]+u$temperature[-nrow(u)])/2, w=diff(u$depth)/depth, na.rm=TRUE))
 	return(tmp)
-
-	
     } else {
+	cat("Choices not supported")
 	return(NULL)
     }
 
