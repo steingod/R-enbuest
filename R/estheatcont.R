f51753d45a54ea3ad98cab81afea1fc77d037af9
steingod <steingod@met.no>
2015-02-24 22:35:31 +0100

    Not fully working estheatcont, but closing. Need interpolation...

--- R/estheatcont.R
+++ R/estheatcont.R
@@ -41,17 +41,52 @@ estheatcont <- function(x, integrationdepth=500) {
 
     # Estimate mean values between depths and weight these according to
     # the number of meters the observations represent.
-    mytime <- strptime(paste(x$data$Date,x$data$Time,x$data$Time_Zone),
-	format="%Y-%m-%d %H:%M UTC")
-    myindex <- as.integer(format(mytime,"%Y%m"))
-    meantemp <- tapply(x$data$Temperature,list(myindex,x$data$Depth),mean,na.rm=T)
-    return(list(id1=levels(factor(myindex)),id2=levels(factor(x$data$Depth)),id3=meantemp))
+    #
+    # Originally the mean value between two and two depths were created by
+    # weighting against the number of meters these observations represent.
+    # Then these weighted mean values were summarised and divided by the
+    # sum of weights to find the weighted mean value for the water column.
+    #
+    # Mean temperatures are currently not weighted, must be redone...
+    # Should split in two tasks, first create weighted mean for each time
+    # step, then create monthly means...
+
+    # Discard observations outside the integration depth
+    mydata <- x[x$depth <= integrationdepth,]
+
+    # Do the vertical weighted mean
+    myindex1 <- as.integer(format(mydata$time,"%Y%j"))
+    mydata$timeid <- myindex1
+    tmp <- split(mydata,mydata$timeid)
+    tmp <- lapply(tmp,function(u) cbind(u, w=weightest(u$depth)))
+    tmp <- lapply(tmp, function(u) weighted.mean(u$temperature,u$w))
+    tmp <- data.frame(do.call("rbind",tmp))
+    colnames(tmp) <- "temperature"
+
+    # Do the monthly means
+    tmp$timeid <- strftime(
+               strptime(rownames(tmp),"%Y%j",tz="GMT"),"%Y%m",tz="GMT"
+               )
+    tmp <- data.frame(
+            temperature = tapply(tmp$temperature,tmp$timeid,mean,na.rm=T))
+    tmp$time <- strptime(paste(rownames(tmp),"15",sep=""),"%Y%m%d",tz="GMT")
+    return(tmp)
 
     # Estimate the difference in heat content between two consequtive time
-    # steps. Based on monthly means.
-    #deltat <- ??
+    # steps (monthly means). Based on monthly means.
+    deltat <- diff(tmp$temperature,1,2)
     delta <- cp*rho*integrationdepth*deltat
-    delatq <- delat/(30.*24*3600)
+    tmp$deltaq <- delta/(30.*24*3600)
 
-    return(data.frame(time=mytime,heatcontent=deltaq))
+    return(tmp)
+}
+
+weightest <- function(u) {
+  n <- length(u)
+  w <- if (n == 1) {
+      1 
+  } else {
+      c((u[2]-u[1])/2, diff(u,lag=2), (u[n]-u[n-1])/2)
+  }
+  return(w/u[length(u)])
 }
